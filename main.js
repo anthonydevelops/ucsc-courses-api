21c2501a74616e5875f9afb4db69d749226c617d
anthonydevelops <anthonydevelops@gmail.com>
2018-11-05 23:16:29 -0800

    Creating term search function still, in progress

--- main.js
+++ main.js
@@ -1,36 +1,7 @@
 const mongoose = require("mongoose");
 const express = require("express");
-const search = require("./lib/courseQuery").getCourse;
-
-const majorCourses = [
-  "cmps12a",
-  "cmps12l",
-  "cmps5j",
-  "cmps11",
-  "cmpe13",
-  "cmpe13l",
-  "cmps12b",
-  "cmps12m",
-  "cmps13h",
-  "cmps13l",
-  "math19a",
-  "math20a",
-  "math19b",
-  "math20b",
-  "math23a",
-  "ams10",
-  "math21",
-  "cmpe16",
-  "cmpe12",
-  "cmpe12l",
-  "cmpe110",
-  "cmps101",
-  "cmps111",
-  "cmps102",
-  "cmps112",
-  "ams131",
-  "cmpe107"
-];
+const courseQuery = require("./lib/courseQuery").getCourse;
+const termQuery = require("./lib/termQuery").getCoursesPerTerm;
 
 const app = express();
 
@@ -47,9 +18,9 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
-app.get("/test", async (req, res) => {
+app.get("/test/course", async (req, res) => {
   try {
-    const courseData = await search("ams131", "2018 Fall Quarter");
+    const courseData = await courseQuery("ams131", "2018 Fall Quarter");
     for (let i = 0; i < courseData.length; i++) {
       const course = new Courses({
         courseID: courseData[i][0].courseID,
@@ -64,6 +35,15 @@ app.get("/test", async (req, res) => {
       // course.save().then(console.log(`Saving ${i} documents ...`));
     }
     res.send(courseData);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+app.get("/test/courses/all", async (req, res) => {
+  try {
+    const termData = await termQuery("O", "2018 Fall Quarter");
+    res.send(termData);
   } catch (e) {
     console.log(e);
   }
